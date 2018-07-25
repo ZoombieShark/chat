8a4fd1cf76bd08fc4e6d48c2488eb7660d947223
Vadim <38106804+ZoombieShark@users.noreply.github.com>
2018-07-25 19:05:57 +0500

    v.1.2

--- client.py
+++ client.py
@@ -19,35 +19,42 @@ def status():  # функция для аутентификации
     answer = ','.join(answer)
     return answer
 
-status()
-
 def receive():
     """Обрабатывает прием сообщений."""
     client_socket.send(answer.encode())
     if client_socket.recv(BUFSIZ) == bytes('Authentication Error', 'utf-8'):
         print('Authentication Error')
         client_socket.close()
+
     while True:
         try:
             print(client_socket.recv(BUFSIZ).decode("utf8"))
         except OSError:  # Возможно, клиент покинул чат.
             quit()
 
-
-
 def send():
     """Обрабатываем отправку сообщения"""
-    while True:
-        msg = input()
-        client_socket.send(bytes(msg, "utf8"))
-        if msg == "{quit}":
-            client_socket.close()
-            quit()
+    try:
+        while True:
+            msg = input()
+            client_socket.send(bytes(msg, "utf8"))
+            if msg == "{quit}":
+                client_socket.close()
+                quit()
+    except:
+        client_socket.close()
+        quit()
 
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+status()
 
-PORT = 9999
+if not PORT:
+    PORT = 9997
+else:
+    PORT = int(PORT)
+
 BUFSIZ = 1024
-HOST = 'localhost'  # ip сервера
 ADDR = (HOST, PORT)
 
 client_socket = socket(AF_INET, SOCK_STREAM)
